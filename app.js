9f14f10d712670181860b7cef59c319612746d45
NaFis Fuad Mim <nafisnfm@gmail.com>
2022-04-06 17:12:37 +0600

    final

--- app.js
+++ app.js
@@ -10,6 +10,25 @@ function updateProduct(product, price, isIncresing) {
   //update total
   const inputTotal = document.getElementById(product + "-total");
   inputTotal.innerText = caseNumber * price;
+  //total calculation
+  totalCalculation();
+}
+//get input;
+function getInput(product) {
+  const getinput = document.getElementById(product + "-number");
+  const getNumber = parseInt(getinput.value);
+  return getNumber;
+}
+//Total calculation
+function totalCalculation() {
+  const phoneInput = getInput("phone") * 1219;
+  const caseInput = getInput("case") * 59;
+  const subTotal = phoneInput + caseInput;
+  const taxTotal = subTotal / 10;
+  const total = subTotal + taxTotal;
+  document.getElementById("sub-total").innerText = subTotal;
+  document.getElementById("tax-total").innerText = taxTotal;
+  document.getElementById("total").innerText = total;
 }
 //phone
 document.getElementById("phone-plus").addEventListener("click", function () {
