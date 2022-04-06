5b44e95d50166d88f5cc6cfbc7d2691435721666
NaFis Fuad Mim <nafisnfm@gmail.com>
2022-04-06 15:10:44 +0600

    button-complete

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('I am in a separate file')
+function updateProduct(isIncresing) {
+  const caseInput = document.getElementById("case-number");
+  const caseNumber = caseInput.value;
+  if (isIncresing == true) {
+    caseInput.value = parseInt(caseNumber) + 1;
+  } else if (caseNumber > 0) {
+    caseInput.value = parseInt(caseNumber) - 1;
+  }
+}
+
+document.getElementById("case-plus").addEventListener("click", function () {
+  updateProduct(true);
+});
+document.getElementById("case-minus").addEventListener("click", function () {
+  updateProduct(false);
+});
